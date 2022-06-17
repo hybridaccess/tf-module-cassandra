6d110d710e0b3c9d95eaaa5219de4ead54485cc6
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-17 06:56:57 +0100

    update

--- locals.tf
+++ locals.tf
@@ -1,5 +1,9 @@
 locals {
-  cluster_properties = jsondecode(data.external.cassandra.result.properties)
-  seed_node_ips      = local.cluster_properties.seedNodes[*].ipAddress
-  gossipCertificates = local.cluster_properties.gossipCertificates[*].pem
+  #cluster_properties = jsondecode(data.external.cassandra.result.properties)
+  #seed_node_ips      = local.cluster_properties.seedNodes[*].ipAddress
+  #gossipCertificates = local.cluster_properties.gossipCertificates[*].pem
+
+  node_ips_file = "${path.module}/nodes_ips.txt"
+  node_ips_data = jsondecode(data.local_file.node_ips.content)
+  node_ips      = local.node_ips_data[*].ipAddress
 }
