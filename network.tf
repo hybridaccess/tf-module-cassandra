1a3f58d171bfa383a74ebe76db58b69e78d50b03
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-29 21:20:51 +0100

    add role assignment

--- network.tf
+++ network.tf
@@ -4,3 +4,8 @@ resource "azurerm_role_assignment" "this" {
   principal_id         = "7711f534-a5ca-4cef-9c6c-8c389b135048"
 }
 
+resource "azurerm_role_assignment" "this2" {
+  scope                = var.cassandra_vnet_id
+  role_definition_name = "Network Contributor"
+  principal_id         = "a232010e-820c-4083-83bb-3ace5fc29d0b"
+}
